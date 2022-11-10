b571535d0f7389c65f29bd06613b6833299af9c3
Roman Timganov <romatimganov@gmail.com>
2022-11-10 19:50:13 +0400

    added percentile for Non-Epic and None-Story tasks
    I can easilly manipulate them now.

--- main.py
+++ main.py
@@ -18,4 +18,11 @@ columns = ["Issue key", "Issue Type", "Custom field (ToDo datetime)", "Custom fi
 df = pd.read_csv("/Users/rtimganov/Downloads/FH Data/LIS3(10-14S).csv", usecols=columns,
                  parse_dates=["Custom field (ToDo datetime)", "Custom field (InTesting datetime)"])
 df["To Test"] = (df["Custom field (InTesting datetime)"] - df["Custom field (ToDo datetime)"]).dt.days
-print(df.to_string())
+df_test_85 = df["To Test"].quantile(0.85)
+#print(df.to_string())
+#print("85% percentile:", df_test_85)
+
+df_test = df[df["Issue Type"] != "Epic"]
+df_test_true = df_test[df_test["Issue Type"] != "Story"]
+print(df_test_true)
+print("85%% = ", df_test_true["To Test"].quantile(0.85))
