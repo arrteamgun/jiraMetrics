d3ac415d31cd16d678c053a5888bb124e82956ee
Roman Timganov <romatimganov@gmail.com>
2022-11-10 19:12:15 +0400

    files added to repository

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import datetime as dt
+import numpy as np
+from matplotlib import pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+'''columns = ["Issue key", "Custom field (Analysis datetime)",
+           "Custom field (Approve datetime)", "Custom field (InProgress datetime)",
+           "Custom field (InTesting datetime)",
+           "Custom field (Planning datetime)", "Custom field (Prod datetime)",
+           "Custom field (ReadyForPlanning datetime)", "Custom field (ReadyForQA datetime)",
+           "Custom field (ReadyForRelease datetime)", "Custom field (Release datetime)",
+           "Custom field (ToDo datetime)"]'''
+
+columns = ["Issue key", "Custom field (ToDo datetime)", "Custom field (InTesting datetime)"]
+
+df = pd.read_csv("/Users/rtimganov/Downloads/FH Data/LIS3(10-14S).csv", usecols=columns)
+'''df["To Test"] = (df["Custom field (InTesting datetime)"] - df["Custom field (ToDo datetime)"]).dt.days'''
+print("Contents in csv file:", df)
+plt.plot(df)
+plt.show()
