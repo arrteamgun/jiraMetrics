e4e27fd37e274778215a208b7dec5761e4a60a45
Roman Timganov <romatimganov@gmail.com>
2022-11-15 18:31:23 +0400

    code cleaned up according to tables and print

--- main.py
+++ main.py
@@ -13,16 +13,38 @@ plt.rcParams["figure.autolayout"] = True
            "Custom field (ReadyForRelease datetime)", "Custom field (Release datetime)",
            "Custom field (ToDo datetime)"]'''
 
-columns = ["Issue key", "Issue Type", "Custom field (ToDo datetime)", "Custom field (InTesting datetime)"]
+#columns = ["Issue key", "Issue Type", "Custom field (ToDo datetime)", "Custom field (InTesting datetime)", "Custom field (Story Points)"]
 
-df = pd.read_csv("/Users/rtimganov/Downloads/FH Data/LIS3(10-14S).csv", usecols=columns,
-                 parse_dates=["Custom field (ToDo datetime)", "Custom field (InTesting datetime)"])
-df["To Test"] = (df["Custom field (InTesting datetime)"] - df["Custom field (ToDo datetime)"]).dt.days
-df_test_85 = df["To Test"].quantile(0.85)
+
+lis3 = "/Users/rtimganov/Downloads/FH Data/LIS3(10-14S).csv"
+wm4 = "/Users/rtimganov/Downloads/FH Data/Web Mobile 4.csv"
+wm_stories = "/Users/rtimganov/Downloads/FH Data/web stories 3m.csv"
+
+start_dt = "Custom field (Start date)"
+end_dt = "Custom field (End date)"
+ready_dt = "Custom field (Ready date)"
+release_dt = "Custom field (Release datetime)"
+
+columns = ["Issue key", "Summary", start_dt, end_dt, ready_dt, release_dt]
+
+df = pd.read_csv(wm_stories, usecols=columns,
+                 parse_dates=[start_dt, end_dt, ready_dt, release_dt])
+
+df["To Test"] = (df[end_dt] - df[start_dt]).dt.days
+df["To Release"] = (df[ready_dt] - df[end_dt]).dt.days
+df["To Prod"] = (df[release_dt] - df[ready_dt]).dt.days
+df["Total"] = (df[release_dt] - df[start_dt]).dt.days
 #print(df.to_string())
 #print("85% percentile:", df_test_85)
+#df_test = df[df["Issue Type"] == "Story"]
+#df_test_true = df_test[df_test["Issue Type"] != "Story"]
 
-df_test = df[df["Issue Type"] != "Epic"]
-df_test_true = df_test[df_test["Issue Type"] != "Story"]
-print(df_test_true)
-print("85%% = ", df_test_true["To Test"].quantile(0.85))
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print((df[["Summary", "To Test", "To Release", "To Prod", "Total"]].to_markdown()))
+
+print("85%% test = ", df["To Test"].quantile(0.85))
+print("85%% release = ", df["To Release"].quantile(0.85))
+print("85%% prod = ", df["To Prod"].quantile(0.85))
+print("85%% total =", df["Total"].quantile(0.85))
+#to do - prod
+#to do - check filters
